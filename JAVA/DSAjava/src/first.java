2bcfefb78086460de5ec7e403aeacd8a56438de3
Ankit Tayal <tayalankit20@gmail.com>
2024-08-07 18:54:52 +0530

    additional commit 7 Aug 2024

---
+++ JAVA/DSAjava/src/first.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class first {
+    public static void main(String[] args) {
+        System.out.print("My First Program!\n");
+        System.out.println("My First Program!");
+        System.out.println("*\n**\n***\n****\n*****");
+    }
+}
