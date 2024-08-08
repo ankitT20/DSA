5aee5558c8f7c41e75fe71f941c1873ef96a38bc
Ankit Tayal <tayalankit20@gmail.com>
2024-08-09 03:29:19 +0530

    additional commit 9 Aug 2024

---
+++ JAVA/DSAjava/src/is_prime.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class is_prime {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int n = scn.nextInt();
+
+            int count = 0;
+            // for (int div = 1; div <= n; div++) {
+            for (int div = 2; div * div <= n; div++) {
+                if (n % div == 0) {
+                    count++;
+                    break;
+                }
+            }
+                
+            // if (count == 2) {
+            if (count == 0) {
+                System.out.println("prime");
+            } else {
+                System.out.println("not prime");
+            }
+        }
+
+
+    }
+}
--- JAVA/DSAjava/src/prime.java
+++ JAVA/DSAjava/src/prime.java
@@ -1,26 +1,13 @@
 import java.util.*;
+
 public class prime {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int t = scn.nextInt();
+        int low = scn.nextInt();
+        int high = scn.nextInt();
 
-        for (int i = 0; i < t; i++) {
-            int n = scn.nextInt();
-
-            int count = 0;
-            for (int div = 1; div <= n; div++) {
-                if (n % div == 0) {
-                    count++;
-                }
-            }
-                
-            if (count == 2) {
-                System.out.println("prime");
-            } else {
-                System.out.println("not prime");
-            }
+        for (int n = low; n <= high; n++) {
+            System.out.println(n);
         }
-
-
     }
 }
