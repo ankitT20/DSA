592b94d0dfeb0fd937845c6dd3f03651800c99e0
Ankit Tayal <tayalankit20@gmail.com>
2024-08-11 23:56:23 +0530

    additional commit 11 Aug 2024

---
+++ JAVA/DSAjava/src/inverse_number.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class inverse_number {
+    // inverse of a number 15234 = 41235
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+
+    }
+}
+// 
