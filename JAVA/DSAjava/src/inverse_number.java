226a85e06f060893fc744de7ce9ff3eab653c04a
Ankit Tayal <tayalankit20@gmail.com>
2024-08-11 23:59:50 +0530

    additional commit 11 Aug 2024

--- JAVA/DSAjava/src/inverse_number.java
+++ JAVA/DSAjava/src/inverse_number.java
@@ -6,7 +6,22 @@ public class inverse_number {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
 
+        int inv = 0;
+        int op = 1;
+        while (n != 0) {
+            int od = n % 10;
+            int id = op;
+            int ip = od;
+
+            // make changes to inv using ip and id
+            inv = inv + id * (int)Math.pow(10, ip - 1)
+
+            n = n / 10;
+            op++;
+        }
+
+        System.out.println(inv);
+
 
     }
 }
-// 
