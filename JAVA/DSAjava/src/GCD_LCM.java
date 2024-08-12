ed8579b229a8fca66df846b1d470b4342567a1b8
Ankit Tayal <tayalankit20@gmail.com>
2024-08-12 05:52:48 +0530

    additional commit 12 Aug 2024

---
+++ JAVA/DSAjava/src/GCD_LCM.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class GCD_LCM {
+    // GCD and LCM L28
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int num1 = scn.nextInt();
+        int num2 = scn.nextInt();
+    }
+}
