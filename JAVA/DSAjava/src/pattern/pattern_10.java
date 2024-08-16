ad9067502a10256e7b2dff25e9ee50cff3c2b28a
Ankit Tayal <tayalankit20@gmail.com>
2024-08-16 23:57:53 +0530

    additional commit 16 Aug 2024

--- JAVA/DSAjava/src/pattern/pattern_10.java
+++ JAVA/DSAjava/src/pattern/pattern_10.java
@@ -8,6 +8,22 @@ public class pattern_10 {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
 
-        
+        int os = n / 2;
+        int is = -1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= os; j++) {
+                System.out.print("\t");
+            }
+            System.out.println("*\t");
+
+            for (int j = 1; j <= is; j++) {
+                System.out.print("\t");
+            }
+
+            if (i > 1 && i < n) {
+                System.out.print("*\t");
+            }
+        }
     }
 }
