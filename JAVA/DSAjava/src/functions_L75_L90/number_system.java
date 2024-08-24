33c3955e85b65edb6796f1f1ef0b7ba4375c796f
Ankit Tayal <tayalankit20@gmail.com>
2024-08-24 23:46:53 +0530

    additional commit 24 Aug 2024

--- JAVA/DSAjava/src/functions_L75_L90/number_system.java
+++ JAVA/DSAjava/src/functions_L75_L90/number_system.java
@@ -3,6 +3,7 @@ package functions_L75_L90;
 import java.util.*;
 
 public class number_system {
+    // Introduction to Number System   L 78
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
