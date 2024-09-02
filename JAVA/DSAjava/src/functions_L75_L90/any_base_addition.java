e7cf1b67cb5d5e72202fce820dd8d6d0e8027969
Ankit Tayal <tayalankit20@gmail.com>
2024-09-02 23:38:49 +0530

    additional commit 2 Sept 2024

--- JAVA/DSAjava/src/functions_L75_L90/any_base_addition.java
+++ JAVA/DSAjava/src/functions_L75_L90/any_base_addition.java
@@ -1,8 +1,15 @@
 package functions_L75_L90;
 
+import java.util.Scanner;
+
 public class any_base_addition {
     // Any base addition   L 86
     public static void main(String[] args) {
-        
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int b1 = scn.nextInt();
+        int b2 = scn.nextInt();
+        int d = getValue(n, b1, b2);
+        System.out.println(d);
     }
 }
