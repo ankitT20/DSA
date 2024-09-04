1e5e6ef6fae15befad3fac18fa0b03eb0ade3c2e
Ankit Tayal <tayalankit20@gmail.com>
2024-09-05 01:35:09 +0530

    additional commit 5 Sept 2024

---
+++ JAVA/DSAjava/src/arrays_L91_L102/intro_arrays.java
@@ -0,0 +1,15 @@
+package arrays_L91_L102;
+
+import java.util.*;
+
+public class intro_arrays {
+    // Introduction to Arrays   L91
+    public static void main(String[] args) {
+        int[] arr;
+    }
+}
+/* 
+Input:
+Output:
+
+ */
--- JAVA/DSAjava/src/functions_L75_L90/any_base_addition.java
+++ JAVA/DSAjava/src/functions_L75_L90/any_base_addition.java
@@ -14,9 +14,20 @@ public class any_base_addition {
         System.out.println(d);
     }
 
-    public static int getSum(int b, int n1, int n2){
+    public static int getSum(int b, int n1, int n2) {
         int rv = 0;
 
+        // 236 + 754 (base 8), 6 + 4 is 10, 10 = 8*1 + 2
+        // 10/8, quotient is 1, remainder is 2
+        // 10 % 8 = 2
+        // 10 / 8 = 1 (carry)
+
+        // | 10 | 236 , 754 |     (divide by 10)
+        // | 10 | 26_6, 75_4|     (quotient_remainder) (add both remainder 6+4 = 10) as (10/8) (Q=1 carry, R=2) R*10^0
+        // | 10 | 2_3 , 7_5 |     (3+5+1carry = 9) (9/8)(Q=1, R=1)                         R*10^1 (mul remainder by 10^1)
+        // | 10 | 0_2 , 0_7 |     (2+7+1carry = 10) (10/8)(Q=1, R =2)                      R*10^2
+        // |    | 0_0 , 0_0 |     (0+0+1carry = 1) (1/8)(Q=0, R =1) (run till Q = 0)       R*10^3 (add all to get on places _ _ _ _ 1212)
+        
         int c = 0;
         int p = 1;
         while (n1 > 0 || n2 > 0 || c > 0) {
@@ -32,6 +43,7 @@ public class any_base_addition {
             rv += d * p;
             p = p* 10;
         }
+        
         return rv;
     }
 }
