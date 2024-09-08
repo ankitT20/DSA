0f9b6f280bb57b67dd1d96a95104764ba95bf5dc
Ankit Tayal <tayalankit20@gmail.com>
2024-09-08 19:01:39 +0530

    additional commit 8 Sept 2024

--- JAVA/DSAjava/src/arrays_L91_L102/intro_arrays.java
+++ JAVA/DSAjava/src/arrays_L91_L102/intro_arrays.java
@@ -5,15 +5,32 @@ import java.util.*;
 public class intro_arrays {
     // Introduction to Arrays   L91
     public static void main(String[] args) {
-        int[] arr;
+        int[] arr; // Declare array
+        arr = new int[5]; // Size of array is 5
 
-        // 
-        int[] arr1;
-        // 
+        int[] arr1 = new int[5]; // combine above 2 lines
+        // declaration and assignment can be joined
+
+        arr[0] = 33;
+        arr[1] = 47;
+        arr[2] = 59;
+        arr[3] = 67;
+        arr[4] = 98;
+
+        System.out.println(arr.length);
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.println(arr[i]);
+        }
+
     }
 }
 /* 
 Input:
 Output:
-
+5
+33
+47
+59
+67
+98
  */
