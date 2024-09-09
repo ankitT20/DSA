d080e15520b1d8e4be209b9d11d8ed2aaa1cdb38
Ankit Tayal <tayalankit20@gmail.com>
2024-09-09 23:37:26 +0530

    additional commit 9 Sept 2024

--- JAVA/DSAjava/src/Stacks_L103_L144/intro_stacks.java
+++ JAVA/DSAjava/src/Stacks_L103_L144/intro_stacks.java
@@ -5,7 +5,10 @@ import java.util.*;
 public class intro_stacks {
     // Introduction to Stacks and its usage   L 103
     public static void main(String[] args) {
-        
+        Stack<Integer> st = new Stack<>();
+
+        st.push(10);
+        st.push(20);
     }
 }
 /* 
