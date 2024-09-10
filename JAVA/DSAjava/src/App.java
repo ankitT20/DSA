9f763fc71c39237b9a09c775037055e512f973d4
Ankit Tayal <tayalankit20@gmail.com>
2024-09-10 23:34:10 +0530

    additional commit 10 Sept 2024

--- JAVA/DSAjava/src/App.java
+++ JAVA/DSAjava/src/App.java
@@ -7,7 +7,7 @@ public class App {
 }
 /* 
 
-c:; cd 'c:\Users\ANKIT\Documents\VScode\DSA'; git add .; git commit -a -m "additional commit 9 Sept 2024"; git push -u origin main;
+c:; cd 'c:\Users\ANKIT\Documents\VScode\DSA'; git add .; git commit -a -m "additional commit 10 Sept 2024"; git push -u origin main;
 
 */
 
--- JAVA/DSAjava/src/Stacks_L103_L144/intro_stacks.java
+++ JAVA/DSAjava/src/Stacks_L103_L144/intro_stacks.java
@@ -9,6 +9,19 @@ public class intro_stacks {
 
         st.push(10);
         st.push(20);
+        System.out.println(st);
+        st.push(30);
+        System.out.println(st);
+        st.push(40);
+        System.out.println(st);
+        System.out.println(st.peek() + " " + st.size());
+        st.pop();
+        System.out.println(st.peek() + " " + st.size());
+        st.pop();
+        System.out.println(st.peek() + " " + st.size());
+        st.pop();
+        System.out.println(st.peek() + " " + st.size());
+        st.pop();
     }
 }
 /* 
