ee9ff060a91eef8435b2b38cad348d762f40f5b7
Ankit Tayal <tayalankit20@gmail.com>
2024-09-27 23:53:53 +0530

    additional commit 27 Sept 2024

--- JAVA/DSAjava/src/Stacks_L103_L144/balanced_brackets.java
+++ JAVA/DSAjava/src/Stacks_L103_L144/balanced_brackets.java
@@ -6,9 +6,36 @@ import java.util.*;
 public class balanced_brackets {
     // Balanced Brackets   L107
     public static void main(String[] args) {
-        // 
-        // 
-        // 
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else if (ch == ')') {
+                boolean val = handleClosing(st, '(');
+                if (val == false) {
+                    System.out.println(val);
+                    return;
+                }
+            } else if (ch == '}') {
+                boolean val = handleClosing(st, '{');
+                if (val == false) {
+                    System.out.println(val);
+                    return;
+                }
+            } else if (ch == '[') {
+                boolean val = handleClosing(st, ']');
+                if (val == false) {
+                    System.out.println(val);
+                    return;
+                }
+            } else {
+                
+            }
+        }
     }
 }
 /* 
