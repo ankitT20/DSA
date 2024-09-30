fd69b465229b94decae29ead0e1588da6e0ecc02
Ankit Tayal <tayalankit20@gmail.com>
2024-10-01 02:57:19 +0530

    additional commit 1 Oct 2024

--- JAVA/DSAjava/src/App.java
+++ JAVA/DSAjava/src/App.java
@@ -6,20 +6,63 @@ public class App {
     }
 }
 /* 
-
-c:; cd 'c:\Users\ANKIT\Documents\VScode\DSA'; git add .; git commit -a -m "additional commit 30 Sept 2024"; git push -u origin main;
-// 
+c:; cd 'c:\Users\ANKIT\Documents\VScode\DSA'; git add .; git commit -a -m "additional commit 1 Oct 2024"; git push -u origin main;
 
 */
-
 // What? How? Code? Why?
-/* 
-import java.io.*;
-import java.util.*;
-
- */
 /* 
 Input:
 Output:
+
+ */
+
+/* 
+import java.io.*;
+import java.util.*;
+ */
+
+
+//  https://web.archive.org/web/20230927015548/https://www.pepcoding.com/resources/online-java-foundation/
+/* 
+
+            Modules and Lectures
+
+1. Basics of Programming 
+    JAVA for C++ Coders
+    Getting Started
+    Patterns
+    Heap 2
+    Function and Arrays
+    2D Arrays
+    String, String Builder and ArrayList
+
+2. Recursion and Backtracking 
+    Introduction to Recursion
+    Recursion in Arrays
+    Recursion with Arraylists
+    Recursion on the way up
+    Recursion Backtracking
+
+3. Dynamic Programming 
+    Dynamic Programming and Greedy
+    Time and Space Complexity
+
+4. OOPS  
+    OOPS
+
+5. Basic Data Structures 
+    Stacks and Queues
+    Linked Lists
+    Generic Tree
+    Binary Tree
+    Binary Search Tree
+
+6. Advanced Data Structure 
+    Hashmap And Heap
+    Graphs
+
+7. Miscellaneous 
+    JAVA for C++ Coders
+    LeetCode
 
  */
