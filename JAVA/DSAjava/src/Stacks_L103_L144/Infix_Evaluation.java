8293ac69d1e161001e60d4f923e61fe89b70ea45
Ankit Tayal <tayalankit20@gmail.com>
2024-10-01 03:32:03 +0530

    additional commit 1 Oct 2024

---
+++ JAVA/DSAjava/src/Stacks_L103_L144/Infix_Evaluation.java
@@ -0,0 +1,33 @@
+package Stacks_L103_L144;
+import java.io.*;
+import java.util.*;
+
+public class Infix_Evaluation {
+    // Infix Evaluation   L 118
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String exp = br.readLine();
+    
+        // code
+        
+    }
+}
+/* 
+Input:
+2 + 6 * 4 / 8 - 3
+Output:
+2
+ */
+
+/* 
+1. You are given an infix expression.
+2. You are required to evaluate and print it's value.
+
+Constraints
+1. Expression is balanced
+2. The only operators used are +, -, *, /
+3. Opening and closing brackets - () - are used to impact precedence of operations
+4. + and - have equal precedence which is less than * and /. * and / also have equal precedence.
+5. In two operators of equal precedence give preference to the one on left.
+6. All operands are single digit numbers.
+ */
