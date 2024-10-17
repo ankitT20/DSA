4ccb8f5436d6e00dcd4f0e8e16febb84fcb94967
Ankit Tayal <tayalankit20@gmail.com>
2024-10-17 23:40:00 +0530

    additional commit 17 Oct 2024

--- JAVA/DSAjava/src/Stacks_L103_L144/Smallest_Number_Following_Pattern.java
+++ JAVA/DSAjava/src/Stacks_L103_L144/Smallest_Number_Following_Pattern.java
@@ -2,6 +2,7 @@ package Stacks_L103_L144;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Smallest_Number_Following_Pattern {
     // Smallest Number Following Pattern   L 130
@@ -10,6 +11,29 @@ public class Smallest_Number_Following_Pattern {
         String str = br.readLine();
     
         // code
+        Stack <Integer> st = new Stack<>();
+        int num = 1;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch == 'd') {
+                st.push(num);
+                num++;
+            } else {
+                st.push(num);
+                num++;
+
+                while (st.size() > 0) {
+                    System.out.print(st.pop());
+                }
+            }
+        }
+
+        st.push(num);
+        while (st.size() > 0) {
+            System.out.print(st.pop());
+        }
     }
 }
 /* 
