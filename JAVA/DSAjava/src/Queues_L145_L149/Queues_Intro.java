0f51b45ad1420582b04546bf51905acaa1e8448b
Ankit Tayal <tayalankit20@gmail.com>
2024-10-19 02:53:40 +0530

    additional commit 19 Oct 2024

---
+++ JAVA/DSAjava/src/Queues_L145_L149/Queues_Intro.java
@@ -0,0 +1,23 @@
+package Queues_L145_L149;
+import java.io.*;
+import java.util.*;
+
+public class Queues_Intro {
+    // Queues | Introduction and Usage   L 145
+    public static void main(String[] args) {
+        Queue<Integer> que = new ArrayDeque<>();
+
+        que.add(10);
+        System.out.println(que);
+        que.add(20);
+        System.out.println(que);
+        que.add(30);
+        System.out.println(que);
+
+        System.out.println(que.peek());
+        System.out.println(que);
+
+        System.out.println(que.remove());
+        System.out.println(que);
+    }
+}
