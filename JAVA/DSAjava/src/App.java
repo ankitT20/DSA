9aa5a4238b3f7479b15d5967a9c75c4122fb59bb
Ankit Tayal <tayalankit20@gmail.com>
2024-10-27 05:23:46 +0530

    additional commit 27 Oct 2024

--- JAVA/DSAjava/src/App.java
+++ JAVA/DSAjava/src/App.java
@@ -6,7 +6,7 @@ public class App {
     }
 }
 /* 
-c:; cd 'c:\Users\ANKIT\Documents\VScode\DSA'; git add .; git commit -a -m "additional commit 25 Oct 2024"; git push -u origin main;
+c:; cd 'c:\Users\ANKIT\Documents\VScode\DSA'; git add .; git commit -a -m "additional commit 27 Oct 2024"; git push -u origin main;
 
 */
 
--- JAVA/DSAjava/src/Linked_Lists_L150_L222/Linked_List_to_Stack_Adapter.java
+++ JAVA/DSAjava/src/Linked_Lists_L150_L222/Linked_List_to_Stack_Adapter.java
@@ -14,18 +14,32 @@ public class Linked_List_to_Stack_Adapter {
 
         int size() {
             // write your code here
+            return list.size();
         }
 
         void push(int val) {
             // write your code here
+            list.addFirst(val);
         }
 
         int pop() {
             // write your code here
+            if (size() == 0) {        
+                System.out.println("Stack underflow");
+                return -1;        
+            } else {
+                return list.removeFirst();
+            }
         }
 
         int top() {
             // write your code here
+            if (size() == 0) {
+                System.out.println("Stack underflow");
+                return -1; 
+            } else {
+                return list.getFirst();
+            }
         }
     }
 
