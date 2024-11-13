e4796918c4b0b383456ad6fc7a1f77370e1fb583
Ankit Tayal <tayalankit20@gmail.com>
2024-11-13 23:53:43 +0530

    additional commit 13 Nov 2024

--- JAVA/DSAjava/src/Generic_Tree_L214_L310/Remove_Leaves_from_Generic_Trees.java
+++ JAVA/DSAjava/src/Generic_Tree_L214_L310/Remove_Leaves_from_Generic_Trees.java
@@ -2,8 +2,8 @@ package Generic_Tree_L214_L310;
 import java.io.*;
 import java.util.*;
 
-public class Mirror_Generic_Tree {
-    // Mirror a Generic Tree L 253
+public class Remove_Leaves_from_Generic_Trees {
+    // Remove Leaves from a Generic Trees L 256
 
     private static class Node {
         int data;
@@ -127,11 +127,14 @@ public class Mirror_Generic_Tree {
     }
 
     public static void mirror(Node node) {
-        // write your code here
         for (Node child : node.children) {
             mirror(child);
         }
         Collections.reverse(node.children);
+    }
+
+    public static void removeLeaves(Node node) {
+        // write your code here
     }
 
     public static void main(String[] args) throws Exception {
@@ -144,17 +147,16 @@ public class Mirror_Generic_Tree {
         }
 
         Node root = construct(arr);
-        display(root);
-        mirror(root);
+        removeLeaves(root);
         display(root);
     }
 
 }
 /* 
-Mirror A Generic Tree
+Remove Leaves In Generic Tree
 1. You are given a partially written GenericTree class.
-2. You are required to complete the body of mirror function. The function is expected to create a mirror image of the tree. For more details, check out the question video.
-3. Input and Output is managed for you. 
+2. You are required to complete the body of removeLeaves function. The function is expected to remove all leaves from the tree. For more details, check out the question video.
+3. Input and Output is managed for you.
 Input Format
 Input is managed for you
 Output Format
@@ -167,27 +169,8 @@ Sample Input
 10 20 50 -1 60 -1 -1 30 70 -1 80 110 -1 120 -1 -1 90 -1 -1 40 100 -1 -1 -1
 Sample Output
 10 -> 20, 30, 40, .
-20 -> 50, 60, .
-50 -> .
-60 -> .
-30 -> 70, 80, 90, .
-70 -> .
-80 -> 110, 120, .
-110 -> .
-120 -> .
-90 -> .
-40 -> 100, .
-100 -> .
-10 -> 40, 30, 20, .
-40 -> 100, .
-100 -> .
-30 -> 90, 80, 70, .
-90 -> .
-80 -> 120, 110, .
-120 -> .
-110 -> .
-70 -> .
-20 -> 60, 50, .
-60 -> .
-50 -> .
+20 -> .
+30 -> 80, .
+80 -> .
+40 -> .
  */
