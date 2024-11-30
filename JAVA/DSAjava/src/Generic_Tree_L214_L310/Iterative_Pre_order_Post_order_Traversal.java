bc41ec9c230911b30eaf411d8fe4255eae89c57c
Ankit Tayal <tayalankit20@gmail.com>
2024-11-30 19:04:43 +0530

    additional commit 30 Nov 2024

--- JAVA/DSAjava/src/Generic_Tree_L214_L310/Iterative_Pre_order_Post_order_Traversal.java
+++ JAVA/DSAjava/src/Generic_Tree_L214_L310/Iterative_Pre_order_Post_order_Traversal.java
@@ -48,8 +48,40 @@ public class Iterative_Pre_order_Post_order_Traversal {
         return root;
     }
 
+    static class Pair {
+        Node node;
+        int state;
+
+        Pair(Node node, int state) {
+            this.node = node;
+            this.state = state;
+        }
+    }
     public static void IterativePreandPostOrder(Node node) {
         // write your code here
+        Stack<Pair> st = new Stack<>();
+        st.push(new Pair(node, -1));
+
+        String pre = "";
+        String post = "";
+        while (st.size() > 0) {
+            Pair top = st.peek();
+            if (top.state == -1) {
+                pre += top.node.data + " ";
+                top.state++;
+            } else if (top.state == top.node.children.size()) {
+                post += top.node.data + " ";
+                st.pop();
+            } else {
+                Pair cp = new Pair(top.node.children.get(top.state), -1);
+                st.push(cp);
+
+                top.state++;
+            }
+        }
+
+        System.out.println(pre);
+        System.out.println(post);
     }
 
     public static void main(String[] args) throws Exception {
