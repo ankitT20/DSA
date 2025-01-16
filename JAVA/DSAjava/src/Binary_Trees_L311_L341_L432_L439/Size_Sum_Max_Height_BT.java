7ec67c8d3ef7dadbcf6af046b7ca8e563129e0c4
Ankit Tayal <tayalankit20@gmail.com>
2025-01-17 03:00:19 +0530

    additional commit 17 Jan 2025 Binary Tree

--- JAVA/DSAjava/src/Binary_Trees_L311_L341_L432_L439/Size_Sum_Max_Height_BT.java
+++ JAVA/DSAjava/src/Binary_Trees_L311_L341_L432_L439/Size_Sum_Max_Height_BT.java
@@ -27,6 +27,44 @@ public class Size_Sum_Max_Height_BT {
         }
     }
 
+    public static Node construct(Integer[] arr) {
+        Node root = new Node(arr[0], null, null);
+        Pair rtp = new Pair(root, 1);
+
+        Stack<Pair> st = new Stack<>();
+        st.push(rtp);
+
+        int idx = 0;
+        while (st.size() > 0) {
+            Pair top = st.peek();;
+            if (top.state == 1) {
+                idx++;
+                if (arr[idx] != null) {
+                    top.node.left = new Node(arr[idx], null, null);
+                    Pair lp = new Pair(top.node.left, 1);
+                    st.push(lp);
+                } else {
+                    top.node.left = null;
+                }
+                top.state++;
+            } else if (top.state == 2) {
+                idx++;
+                if (arr[idx] != null) {
+                    top.node.right = new Node(arr[idx], null, null);
+                    Pair rp = new Pair(top.node.right, 1);
+                    st.push(rp);
+                } else {
+                    top.node.right = null;
+                }
+                top.state++;
+            } else {
+                st.pop();
+            }
+        }
+
+        return root;
+    }
+
     public static void display(Node node) {
         if (node == null) {
             return;
@@ -77,7 +115,7 @@ public class Size_Sum_Max_Height_BT {
 
     public static int height(Node node) {
         if (node == null) {
-            return -1;
+            return -1;  // -1 for edges, 0 for nodes
         }
 
         int lh = height(node.left);
@@ -85,44 +123,40 @@ public class Size_Sum_Max_Height_BT {
 
         return Math.max(lh, rh) + 1;
     }
-    
-    public static void main(String[] args) {
-        Integer[] arr = { 50, 25, 12, null, null, 37, 30, null, null, null, 75, 62, null, 70, null, null, 87, null, null};
-        Node root = new Node(arr[0], null, null);
-        Pair rtp = new Pair(root, 1);
 
-        Stack<Pair> st = new Stack<>();
-        st.push(rtp);
-
-        int idx = 0;
-        while (st.size() > 0) {
-            Pair top = st.peek();;
-            if (top.state == 1) {
-                idx++;
-                if (arr[idx] != null) {
-                    top.node.left = new Node(arr[idx], null, null);
-                    Pair lp = new Pair(top.node.left, 1);
-                    st.push(lp);
-                } else {
-                    top.node.left = null;
-                }
-                top.state++;
-            } else if (top.state == 2) {
-                idx++;
-                if (arr[idx] != null) {
-                    top.node.right = new Node(arr[idx], null, null);
-                    Pair rp = new Pair(top.node.right, 1);
-                    st.push(rp);
-                } else {
-                    top.node.right = null;
-                }
-                top.state++;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[n];
+        String[] values = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            if (values[i].equals("n") == false) {
+                arr[i] = Integer.parseInt(values[i]);
             } else {
-                st.pop();
+                arr[i] = null;
             }
         }
 
-        display(root);
+        Node root = construct(arr);
 
+        int size = size(root);
+        int sum = sum(root);
+        int max = max(root);
+        int height = height(root);
+        System.out.println(size);
+        System.out.println(sum);
+        System.out.println(max);
+        System.out.println(height);
     }
+
 }
+/* 
+Input:
+19
+50 25 12 n n 37 30 n n n 75 62 n 70 n n 87 n n
+Output:
+9
+448
+87
+3
+ */
---
+++ JAVA/DSAjava/src/Binary_Trees_L311_L341_L432_L439/Traversals_BT.java
@@ -0,0 +1,5 @@
+package Binary_Trees_L311_L341_L432_L439;
+
+public class Traversals_BT {
+    // Traversals in a Binary Tree   L 318
+}
