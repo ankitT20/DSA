bd856a053005dea731557229353f02edf3f4eea8
Ankit Tayal <tayalankit20@gmail.com>
2025-01-30 00:39:55 +0530

    additional commit 30 Jan 2025

--- JAVA/DSAjava/src/App.java
+++ JAVA/DSAjava/src/App.java
@@ -6,7 +6,7 @@ public class App {
     }
 }
 /* 
-c:; cd 'c:\Users\ANKIT\Documents\VScode\DSA'; git add .; git commit -a -m "additional commit 29 Jan 2025"; git push -u origin main;
+c:; cd 'c:\Users\ANKIT\Documents\VScode\DSA'; git add .; git commit -a -m "additional commit 30 Jan 2025"; git push -u origin main;
 */
 
 /* 
---
+++ JAVA/DSAjava/src/TwoD_Arrays_L342_L359/Shell_Rotate.java
@@ -0,0 +1,97 @@
+package TwoD_Arrays_L342_L359;
+import java.io.*;
+import java.util.*;
+@SuppressWarnings("unused")
+
+public class Shell_Rotate {
+    // Shell Rotate   L 353
+    public static void main(String[] args) throws Exception {
+        
+    }
+
+    public static void display(int[][] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+/* 
+Ring Rotate
+You are given a n*m matrix where n are the number of rows and m are the number of columns. You are also given n*m numbers representing the elements of the matrix.
+You will be given a ring number 's' representing the ring of the matrix. For details, refer to image.
+
+shell-rotate
+
+You will be given a number 'r' representing number of rotations in an anti-clockwise manner of the specified ring.
+You are required to rotate the 's'th ring by 'r' rotations and display the rotated matrix.
+
+Input Format
+A number n
+A number m
+e11
+e12..
+e21
+e22..
+.. n * m number of elements of array a
+A number s
+A number r
+Output Format
+output is handled by display function
+
+Constraints
+1 <= n <= 10^2
+1 <= m <= 10^2
+-10^9 <= e11, e12, .. n * m elements <= 10^9
+0 < s <= min(n, m) / 2
+-10^9 <= r <= 10^9
+
+Sample Input
+5
+7
+11
+12
+13
+14
+15
+16
+17
+21
+22
+23
+24
+25
+26
+27
+31
+32
+33
+34
+35
+36
+37
+41
+42
+43
+44
+45
+46
+47
+51
+52
+53
+54
+55
+56
+57
+2
+3
+Sample Output
+11 12 13 14 15 16 17
+21 25 26 36 46 45 27
+31 24 33 34 35 44 37
+41 23 22 32 42 43 47
+51 52 53 54 55 56 57
+ */
