451e1f25a5a50698e10f392478ab9a2da579e04c
Ankit Tayal <tayalankit20@gmail.com>
2025-01-30 23:41:52 +0530

    additional commit 30 Jan 2025

--- JAVA/DSAjava/src/TwoD_Arrays_L342_L359/Shell_Rotate.java
+++ JAVA/DSAjava/src/TwoD_Arrays_L342_L359/Shell_Rotate.java
@@ -6,7 +6,29 @@ import java.util.*;
 public class Shell_Rotate {
     // Shell Rotate   L 353
     public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+
+        int[][] arr = new int[n][m];
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                arr[i][j] = scn.nextInt();
+            }
+        }
         
+        int s = scn.nextInt();
+        int r = scn.nextInt();
+        scn.close();
+
+        rotateShell(arr, s, r);
+        display(arr);
+    }
+
+    public static void rotateShell(int[][] arr, int s, int r){
+        int[] oned = fillOnedFromShell(arr, s);
+        rotate(oned, r);
+        fillShellFromOned(arr, s, oned);
     }
 
     public static void display(int[][] arr){
