20f5977f533b9f8e82afe1034f0c243e2ac1fd88
Ankit Tayal <tayalankit20@gmail.com>
2025-02-12 06:19:49 +0530

    additional commit 12 Feb 2025

--- JAVA/DSAjava/src/Basics_of_Programming/arrays_L91_L102_L360_L374/First_and_Last_Index.java
+++ JAVA/DSAjava/src/Basics_of_Programming/arrays_L91_L102_L360_L374/First_and_Last_Index.java
@@ -6,7 +6,52 @@ import java.util.*;
 public class First_and_Last_Index {
     // First and Last Index   L 374
     public static void main(String[] args) throws Exception {
-        
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        int data = scn.nextInt();
+        scn.close();
+
+        int low = 0;
+        int high = arr.length - 1;
+        int firstIndex = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (data > arr[mid]) {
+                low = mid + 1;
+            } else if (data < arr[mid]) {
+                high = mid - 1;
+            } else {
+                firstIndex = mid;
+                high = mid - 1;
+            }
+        }
+
+        System.out.println(firstIndex);
+
+        low = 0;
+        high = arr.length - 1;
+        int lastIndex = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (data > arr[mid]) {
+                low = mid + 1;
+            } else if (data < arr[mid]) {
+                high = mid - 1;
+            } else {
+                lastIndex = mid;
+                low = mid + 1;
+            }
+        }
+
+        System.out.println(lastIndex);
     }
 }
 /* 
