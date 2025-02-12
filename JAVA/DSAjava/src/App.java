eee04e5975e1d260225dbee1f5bbb1ef41adfef1
Ankit Tayal <tayalankit20@gmail.com>
2025-02-13 02:13:13 +0530

    additional commit 13 Feb 2025

--- JAVA/DSAjava/src/App.java
+++ JAVA/DSAjava/src/App.java
@@ -6,7 +6,7 @@ public class App {
     }
 }
 /* 
-c:; cd 'c:\Users\ANKIT\Documents\VScode\DSA'; git add .; git commit -a -m "additional commit 12 Feb 2025"; git push -u origin main;
+c:; cd 'c:\Users\ANKIT\Documents\VScode\DSA'; git add .; git commit -a -m "additional commit 13 Feb 2025"; git push -u origin main;
 */
 
 /* 
---
+++ JAVA/DSAjava/src/Recursion_and_Backtracking/Introduction_to_Recursion_L375_L385/Print_Increasing.java
@@ -0,0 +1,44 @@
+package Recursion_and_Backtracking.Introduction_to_Recursion_L375_L385;
+import java.io.*;
+import java.util.*;
+@SuppressWarnings("unused")
+
+public class Print_Increasing {
+    // Print Increasing   L 377
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        scn.close();
+        printIncreasing(n);
+    }
+
+    public static void printIncreasing(int n){
+        
+    }
+}
+/* 
+Print Increasing
+1. You are given a positive number n. 
+2. You are required to print the counting from 1 to n.
+3. You are required to not use any loops. Complete the body of print Increasing function to achieve it. Don't change the signature of the function.
+Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+Input Format
+A number n
+Output Format
+1
+2
+3
+..
+n
+Constraints
+1 <= n <= 1000
+
+Sample Input
+5
+Sample Output
+1
+2
+3
+4
+5
+ */
