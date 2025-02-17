0e1a541904375da4e6aa7e9d5859e66f54127d85
Ankit Tayal <tayalankit20@gmail.com>
2025-02-17 18:44:22 +0530

    additional commit 17 Feb 2025

--- JAVA/DSAjava/src/Recursion_and_Backtracking/Introduction_to_Recursion_L375_L385/Print_Decreasing.java
+++ JAVA/DSAjava/src/Recursion_and_Backtracking/Introduction_to_Recursion_L375_L385/Print_Decreasing.java
@@ -13,7 +13,11 @@ public class Print_Decreasing {
     }
 
     public static void printDecreasing(int n){
-        
+        if(n == 0){
+            return;
+        }
+        System.out.println(n);
+        printDecreasing(n - 1);
     }
 }
 /* 
