e9f2b9460677384c833116e40db2a066ba15527a
Ankit Tayal <tayalankit20@gmail.com>
2025-02-19 03:37:11 +0530

    additional commit 19 Feb 2025

--- JAVA/DSAjava/src/Recursion_and_Backtracking/Introduction_to_Recursion_L375_L385/Factorial.java
+++ JAVA/DSAjava/src/Recursion_and_Backtracking/Introduction_to_Recursion_L375_L385/Factorial.java
@@ -1,9 +1,12 @@
 package Recursion_and_Backtracking.Introduction_to_Recursion_L375_L385;
+import java.io.*;
+import java.util.*;
+@SuppressWarnings("unused")
 
 public class Factorial {
     // Factorial   L 380
     public static void main(String[] args) throws Exception {
-        // write your code here
+        
     }
 
     public static int factorial(int n){
