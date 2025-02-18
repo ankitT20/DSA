ad3e017f4a455b823467a3c17abcb9fe5d679aac
Ankit Tayal <tayalankit20@gmail.com>
2025-02-19 00:06:09 +0530

    additional commit 19 Feb 2025

--- JAVA/DSAjava/src/Recursion_and_Backtracking/Introduction_to_Recursion_L375_L385/Factorial.java
+++ JAVA/DSAjava/src/Recursion_and_Backtracking/Introduction_to_Recursion_L375_L385/Factorial.java
@@ -10,3 +10,20 @@ public class Factorial {
         return 0;
     }
 }
+/* 
+Factorial
+1. You are given a number n.
+2. You are required to calculate the factorial of the number. Don't change the signature of factorial function.
+Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is.Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+Input Format
+A number n
+Output Format
+factorial of n
+Constraints
+0 <= n <= 10
+
+Sample Input
+5
+Sample Output
+120
+ */
